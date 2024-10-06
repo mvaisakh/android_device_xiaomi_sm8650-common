7d251d2a9c706242e331094069cd696e5edad58b
Vaisakh Murali <mvaisakh@statixos.com>
2024-10-06 16:11:41 +0530

    sm8650-common: Initial sm8650 makefiles
    Change-Id: I1dad2791673852424f9f7f4ace15620801ab245c

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,21 @@
+#
+# Copyright (c) 2014 The Android Open-Source Project
+# Copyright (c) 2024 StatiXOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/xiaomi/sm8650-common
+
+# Use the non-open-source parts, if they're present
+-include vendor/xiaomi/sm8650-common/BoardConfigVendor.mk
+
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
---
+++ pineapple.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2014 The Android Open-Source Project
+# Copyright (c) 2024 StatiXOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product-if-exists, vendor/xiaomi/sm8650-common/sm8650-common-vendor.mk)
