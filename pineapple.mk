f1d86e44c64749351eb1dc0c5e824ef17369f483
Vaisakh Murali <mvaisakh@statixos.com>
2024-10-09 21:11:17 +0530

    sm8650-common: Inherit vibrator hal product makefile
    Change-Id: I8f1eed9fb79a98e1b663ed3566f94eeaa67a6bf6

--- pineapple.mk
+++ pineapple.mk
@@ -73,3 +73,6 @@ PRODUCT_COPY_FILES += \
 # Verified boot
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
+
+# Vibrator
+$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
