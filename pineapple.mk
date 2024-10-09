f32e3072a6a34195ee380053e03f8e0fe671775b
Vaisakh Murali <mvaisakh@statixos.com>
2024-10-09 18:58:07 +0530

    sm8650-common: Inherit common vabc features
    Change-Id: I2befbc4dc66d3357f7932e2541b59f0370a14e62

--- pineapple.mk
+++ pineapple.mk
@@ -11,3 +11,7 @@ $(call inherit-product-if-exists, vendor/xiaomi/sm8650-common/sm8650-common-vend
 TARGET_BOARD_PLATFORM := pineapple
 PRODUCT_USES_QCOM_HARDWARE := true
 PRODUCT_BOARD_PLATFORM := $(TARGET_BOARD_PLATFORM)
+
+# A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/vabc_features.mk)
