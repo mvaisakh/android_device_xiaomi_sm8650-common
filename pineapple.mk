12bd167c36abb59183e7202d5ff30f06990ab786
Vaisakh Murali <mvaisakh@statixos.com>
2024-10-09 18:16:20 +0530

    sm8650-common: Setup qcom board flags
    Change-Id: I3d1ff6e2b138f202e1fca7180de573be0e8e384c

--- pineapple.mk
+++ pineapple.mk
@@ -6,3 +6,8 @@
 #
 
 $(call inherit-product-if-exists, vendor/xiaomi/sm8650-common/sm8650-common-vendor.mk)
+
+# Board
+TARGET_BOARD_PLATFORM := pineapple
+PRODUCT_USES_QCOM_HARDWARE := true
+PRODUCT_BOARD_PLATFORM := $(TARGET_BOARD_PLATFORM)
