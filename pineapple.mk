78dac5dc5889cf6d0645a06235b0927f59884c11
Vaisakh Murali <mvaisakh@statixos.com>
2024-10-23 17:47:22 +0530

    sm8650-common: Build OSS keymint nxp service
    Change-Id: I689ab228703ee16fd1af95744eabf4a7727645ea

--- pineapple.mk
+++ pineapple.mk
@@ -75,6 +75,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
+PRODUCT_PACKAGES += \
+    android.hardware.authsecret-service.nxp \
+    android.hardware.security.keymint3-service.strongbox.nxp
+
 # MIDI feature
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
