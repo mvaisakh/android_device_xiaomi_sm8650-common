75c92718ac2559b704a55cef3f62bbcc42630477
Vaisakh Murali <mvaisakh@statixos.com>
2024-10-23 19:09:29 +0530

    sm8650-common: Setup init scripts
    Only keep scripts that will be modified in tree.
    Keep all the rest in vendor as most scripts are now proprietary and
    will probably not be modified.
    If modifications are required, the scripts can be imported into the
    tree at a later time.
    
    Change-Id: Ib48eeb09d92225db4afdfcdefc536fe1e4ad8c26

--- pineapple.mk
+++ pineapple.mk
@@ -65,7 +65,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
 PRODUCT_PACKAGES += \
-    fstab.qcom
+    fstab.qcom \
+    init.qcom.rc \
+    init.recovery.qcom.rc \
+    init.target.rc \
+    ueventd.qcom.rc
 
 # IR
 PRODUCT_COPY_FILES += \
