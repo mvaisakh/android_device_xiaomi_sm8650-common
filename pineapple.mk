2032953d31dd203bf8fa60ee356620631cbcb846
Vaisakh Murali <mvaisakh@statixos.com>
2024-10-23 17:54:08 +0530

    sm8650-common: Build OSS AIDL DRM Clearkey service
    Change-Id: I67d6832a2428231eb53c96ed3b2364b66b7e28e9

--- pineapple.mk
+++ pineapple.mk
@@ -48,6 +48,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm-service.clearkey
+
 # Fingerprint feature
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
