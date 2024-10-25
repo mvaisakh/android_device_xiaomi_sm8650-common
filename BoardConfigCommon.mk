3abc490ef3bb72b3649d004b504843eb769b51ce
Vaisakh Murali <mvaisakh@statixos.com>
2024-10-25 17:36:48 +0530

    sm8650-common: Use armv9-a with generic CPU variant
    Change-Id: I6dc1df28bb9dee0dc6f6e158620d6fd746ff7af8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,9 +29,9 @@ AB_OTA_PARTITIONS += \
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv9-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := pineapple
